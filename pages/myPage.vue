8382459354bf152547621be0cf2aa5c837e935cd
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-12-20 22:40:12 +0530

    ZenHighonPhysics is initialized - Yolo :-)

---
+++ pages/myPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-gray-900 text-white flex justify-center text-lg">
+    <h1>Hello from My Page</h1>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
